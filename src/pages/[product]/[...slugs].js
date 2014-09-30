e1a3e3363ff4dafd0511c57b9945bb04fbdeb280
Carlos <carlosbozemandev@gmail.com>
2014-09-30 14:00:00 +0500

    dummy

--- src/pages/[product]/[...slugs].js
+++ src/pages/[product]/[...slugs].js
@@ -74,8 +74,8 @@ export default function Slug(props) {
 
 export async function getStaticProps({ params }) {
     const { product, slugs } = params;
-    const res = await fetch(`https://audiophile-murex.vercel.app/api/products/${product}/${slugs[0]}`);
-    // const res = await fetch(`http://localhost:3000/api/products/${product}/${slugs[0]}`);
+    // const res = await fetch(`https://audiophile-murex.vercel.app/api/products/${product}/${slugs[0]}`);
+    const res = await fetch(`https://jsonplaceholder.typicode.com/todos`);
     const data = await res.json();
     return {
         props: {
